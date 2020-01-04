162d5c5dd166ba528b7cc3aeeee2b3421ffae5cf
minhhung <minhhung19494@gmail.com>
2020-01-04 23:58:47 +0700

    fix Maze create

--- src/Maze/RecursiveDivision.js
+++ src/Maze/RecursiveDivision.js
@@ -1,6 +1,17 @@
 export function recursiveDivision(grid, startNode, finishNode) {
     let orientation = chooseOrientation(grid[0].length, grid.length);
     const wallInOrder = [];
+    for(let i=0; i<grid[0].length; i++){
+        wallInOrder.push(grid[0][i]);
+        wallInOrder.push(grid[grid.length-1][i])
+    }
+    for(let i=1; i<grid.length-1; i++){
+        wallInOrder.push(grid[i][0]);
+        wallInOrder.push(grid[i][grid[0].length-1]);
+    }
+    for(let i = 0; i<wallInOrder.length; i++){
+        wallInOrder[i].isWall = true;
+    }
     divide(grid, 1, 1, grid[0].length - 2, grid.length - 2, orientation, wallInOrder);
     return wallInOrder;
 }
@@ -18,10 +29,10 @@ function chooseOrientation(width, height) {
 function divide(grid, x, y, width, height, orientation, wallInOrder) {
     if (width < 2 || height < 2) return;
     let horizontal = orientation === 'HORIZONTAL';
-    let wx = x + (horizontal ? 0 : Math.floor(Math.random() * (width - 2)) + 1);
-    let wy = y + (horizontal ? Math.floor(Math.random() * (height - 2)) + 1 : 0);
-    let px = wx + (horizontal ? Math.floor(Math.random() * (width - 2)) + 1 : 0);
-    let py = wy + (horizontal ? 0 : Math.floor(Math.random() * (height - 2)) + 1);
+    let wx = x + (horizontal ? 0 : Math.floor(Math.random() * (width - 2)));
+    let wy = y + (horizontal ? Math.floor(Math.random() * (height - 2)) : 0);
+    let px = wx + (horizontal ? Math.floor(Math.random() * (width - 2)) : 0);
+    let py = wy + (horizontal ? 0 : Math.floor(Math.random() * (height - 2)));
     // console.log(wx, wy);
     // console.log(orientation);
 
@@ -31,7 +42,6 @@ function divide(grid, x, y, width, height, orientation, wallInOrder) {
     let countY = wy;
     while (countX < width + x && countY < height + y) {
         if (countX !== px || countY !== py) {
-            console.log(countX, countY);
             grid[countY][countX].isWall = true;
             wallInOrder.push(grid[countY][countX]);
         }
@@ -39,20 +49,13 @@ function divide(grid, x, y, width, height, orientation, wallInOrder) {
         countY += dy;
     }
     console.log(wallInOrder);
-    // let w1, h1 = horizontal ? [width, wy - y] : [wx - x, height];
-    // let w2, h2 = horizontal ? [width, height - wy - 1] : [width - wx - 1, height]
-    // let nx, ny = horizontal ? [x, wy + 1] : [wx + 1, y];
-    // console.log(w1,h1,w2,h2);
-    // console.log(nx, ny);
-    // console.log(nx);
     if (horizontal) {
         divide(grid, x, y, width, wy - y, chooseOrientation(width, wy - y), wallInOrder);
-        divide(grid, x, wy + 1, width, height - wy, chooseOrientation(width, height - wy));
+        divide(grid, x, wy + 1, width, height - wy, chooseOrientation(width, height - wy), wallInOrder);
     } else {
         divide(grid, x, y, wx - x, height, chooseOrientation(wx - x, height), wallInOrder);
         divide(grid, wx + 1, y, width - wx, height, chooseOrientation(width - wx, height), wallInOrder);
     };
-    // divide(grid, x, y, w1, h1, chooseOrientation(w1, h1));
-    // divide(grid, nx, ny, w2, h2, chooseOrientation(w2, h2));
 }
 
+ 
